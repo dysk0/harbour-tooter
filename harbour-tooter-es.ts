9e477c63462074690ec06243c1dad92f95ec08ef
Carlos   Gonzalez <carlosgonz@protonmail.com>
2017-06-07 09:14:45 -0400

    Create harbour-tooter-es.ts

---
+++ harbour-tooter-es.ts
@@ -0,0 +1,133 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name></name>
+    <message id="Logout">
+        <source></source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="Login">
+        <source></source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Browser</name>
+    <message>
+        <source>Open in Browser</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Web mode</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Reading mode</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MainPage</name>
+    <message>
+        <source>Timeline</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Home</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Notifications</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MyList</name>
+    <message>
+        <source>Load more</source>
+        <translation type="unfinished">Cargar más</translation>
+    </message>
+</context>
+<context>
+    <name>Notification</name>
+    <message>
+        <source>metioned you</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>boosted your status</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>favourited your status</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>followed you</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Profile</name>
+    <message>
+        <source>Unfollow</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Follow request sent!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Following</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Mute</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Unmute</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Unblock</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Block</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Statuses</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Favourites</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Follow</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Summary</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Followers</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SecondPage</name>
+    <message>
+        <source>Nested Page</source>
+        <translation>Página anidada</translation>
+    </message>
+    <message>
+        <source>Item</source>
+        <translation>Artículo</translation>
+    </message>
+</context>
+</TS>
