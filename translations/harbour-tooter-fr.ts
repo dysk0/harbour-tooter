0a319e84250746d954071016788f110dcae387eb
Quent-in <pages_quentin@hotmail.com>
2017-06-12 08:57:06 +0200

    i18n French locale file

---
+++ translations/harbour-tooter-fr.ts
@@ -0,0 +1,156 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name></name>
+    <message id="Logout">
+        <source></source>
+        <translation></translation>
+    </message>
+    <message id="Login">
+        <source></source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>Browser</name>
+    <message>
+        <source>Open in Browser</source>
+        <translation>Ouvrir dans le navigateur</translation>
+    </message>
+    <message>
+        <source>Web mode</source>
+        <translation>Mode Web</translation>
+    </message>
+    <message>
+        <source>Reading mode</source>
+        <translation>Mode lecture</translation>
+    </message>
+</context>
+<context>
+    <name>Conversation</name>
+    <message>
+        <source>Content warning!</source>
+        <translation>Contenu sensible !</translation>
+    </message>
+    <message>
+        <source>public</source>
+        <translation>public</translation>
+    </message>
+    <message>
+        <source>unlisted</source>
+        <translation>non-listé</translation>
+    </message>
+	<message>
+        <source>followers only</source>
+        <translation>abonnés uniquement</translation>
+    </message>
+    <message>
+        <source>direct</source>
+        <translation>direct</translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <source>Tooter</source>
+        <translation>Tooter</translation>
+    </message>
+</context>
+<context>
+    <name>MainPage</name>
+    <message>
+        <source>Home</source>
+        <translation>Accueil</translation>
+    </message>
+    <message>
+        <source>Timeline</source>
+        <translation>Fil</translation>
+    </message>
+    <message>
+        <source>Notifications</source>
+        <translation>Notifications</translation>
+    </message>
+    <message>
+        <source>Search</source>
+        <translation>Recherche</translation>
+    </message>
+    <message>
+        <source>New Toot</source>
+        <translation>Nouveau pouet</translation>
+    </message>
+</context>
+<context>
+    <name>MyList</name>
+    <message>
+        <source>Load more</source>
+        <translation>Charger davantage</translation>
+    </message>
+</context>
+<context>
+    <name>Profile</name>
+    <message>
+        <source>Unfollow</source>
+        <translation>Ne plus suivre</translation>
+    </message>
+    <message>
+        <source>Follow request sent!</source>
+        <translation>Demande d’abonnement envoyée !</translation>
+    </message>
+    <message>
+        <source>Following</source>
+        <translation>Abonnements</translation>
+    </message>
+    <message>
+        <source>Mute</source>
+        <translation>Masquer</translation>
+    </message>
+    <message>
+        <source>Unmute</source>
+        <translation>Ne plus masquer</translation>
+    </message>
+    <message>
+        <source>Unblock</source>
+        <translation>Débloquer</translation>
+    </message>
+    <message>
+        <source>Block</source>
+        <translation>Bloquer</translation>
+    </message>
+    <message>
+        <source>Statuses</source>
+        <translation>Statuts</translation>
+    </message>
+    <message>
+        <source>Favourites</source>
+        <translation>Favoris</translation>
+    </message>
+    <message>
+        <source>Follow</source>
+        <translation>Suivre</translation>
+    </message>
+    <message>
+        <source>Summary</source>
+        <translation>Résumé</translation>
+    </message>
+    <message>
+        <source>Followers</source>
+        <translation>Abonnés</translation>
+    </message>
+</context>
+<context>
+    <name>Toot</name>
+    <message>
+        <source>boosted</source>
+        <translation>a partagé</translation>
+    </message>
+    <message>
+        <source>favourited</source>
+        <translation>a ajouté à ses favoris</translation>
+    </message>
+    <message>
+        <source>followed you</source>
+        <translation>vous suit</translation>
+    </message>
+</context>
+</TS>
