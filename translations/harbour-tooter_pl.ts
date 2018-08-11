14065b3f74b9bab16bad30c1406688e6c1daec98
Marcin Mikołajczak <me@m4sk.in>
2018-08-11 17:20:32 +0200

    i18n: Add Polish translation
    Signed-off-by: Marcin Mikołajczak <me@m4sk.in>

---
+++ translations/harbour-tooter_pl.ts
@@ -0,0 +1,307 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="pl_PL">
+<context>
+    <name>API</name>
+    <message>
+        <source>favourited</source>
+        <translation type="unfinished">dodał(a) do ulubionych</translation>
+    </message>
+    <message>
+        <source>followed you</source>
+        <translation>zaczął(-ęła) Cię śledzić</translation>
+    </message>
+    <message>
+        <source>boosted</source>
+        <translation>podbił(a)</translation>
+    </message>
+    <message>
+        <source>said</source>
+        <translation>napisał(a)</translation>
+    </message>
+</context>
+<context>
+    <name>Browser</name>
+    <message>
+        <source>Open in Browser</source>
+        <translation>Otwórz w przeglądarce</translation>
+    </message>
+    <message>
+        <source>Web mode</source>
+        <translation>Tryb sieci</translation>
+    </message>
+    <message>
+        <source>Reading mode</source>
+        <translation>Tryb czytania</translation>
+    </message>
+    <message>
+        <source>Copy URL</source>
+        <translation>Kopiuj URL</translation>
+    </message>
+</context>
+<context>
+    <name>Conversation</name>
+    <message>
+        <source>Conversation</source>
+        <translation>Konwersacja</translation>
+    </message>
+    <message>
+        <source>Content warning!</source>
+        <translation>Ostrzeżenie o zawartości!</translation>
+    </message>
+    <message>
+        <source>public</source>
+        <translation>publiczny</translation>
+    </message>
+    <message>
+        <source>unlisted</source>
+        <translation>niewidoczny</translation>
+    </message>
+    <message>
+        <source>followers only</source>
+        <translation>tylko dla śledzących</translation>
+    </message>
+    <message>
+        <source>direct</source>
+        <translation>bezpośredni</translation>
+    </message>
+    <message>
+        <source>Delete</source>
+        <translation>Usuń</translation>
+    </message>
+    <message>
+        <source>Emojis</source>
+        <translation>Emoji</translation>
+    </message>
+    <message>
+        <source>Tap to insert</source>
+        <translation>Dotknij aby wstawić</translation>
+    </message>
+</context>
+<context>
+    <name>ImageFullScreen</name>
+    <message>
+        <source>Error loading</source>
+        <translation>Błąd ładowania</translation>
+    </message>
+</context>
+<context>
+    <name>ImageUploader</name>
+    <message>
+        <source>The file %1 does not exists</source>
+        <translation>Plik %1 nie istnieje</translation>
+    </message>
+</context>
+<context>
+    <name>LoginPage</name>
+    <message>
+        <source>Login</source>
+        <translation>Zaloguj się</translation>
+    </message>
+    <message>
+        <source>Enter an Mastodon instance URL</source>
+        <translation>Wprowadź adres URL instancji Mastodona</translation>
+    </message>
+    <message>
+        <source>Mastodon is a free, open-source social network. A decentralized alternative to commercial platforms, it avoids the risks of a single company monopolizing your communication. Pick a server that you trust — whichever you choose, you can interact with everyone else. Anyone can run their own Mastodon instance and participate in the social network seamlessly.</source>
+        <translation>Mastodon jest wolną i otwartoźródłową siecią społecznościową. Zdecentralizowana alternatywa dla komercyjnych platform, pozwala uniknąć ryzyka wynikającego z kontroli Twojej komunikacji przez jedną jednostkę. Wybierz serwer któremu ufasz – jakikolwiek wybierzesz, możesz wejść w interakcje z innymi. Każdy może uruchomić własną instancję Mastodona i być członkiem tej sieci społecznościowej.</translation>
+    </message>
+</context>
+<context>
+    <name>MainPage</name>
+    <message>
+        <source>Home</source>
+        <translation>Strona główna</translation>
+    </message>
+    <message>
+        <source>Notifications</source>
+        <translation>Powiadomienia</translation>
+    </message>
+    <message>
+        <source>New Toot</source>
+        <translation>Nowy wpis</translation>
+    </message>
+    <message>
+        <source>Search</source>
+        <translation>Szukaj</translation>
+    </message>
+    <message>
+        <source>@user or #term</source>
+        <translation>@użytkownik lub #tag</translation>
+    </message>
+    <message>
+        <source>Local</source>
+        <translation>Lokalne</translation>
+    </message>
+    <message>
+        <source>Federated</source>
+        <translation>Globalne</translation>
+    </message>
+</context>
+<context>
+    <name>MiniStatus</name>
+    <message>
+        <source>boosted</source>
+        <translation>podbił(a)</translation>
+    </message>
+    <message>
+        <source>favourited</source>
+        <translation>dodał(a) do ulubionych</translation>
+    </message>
+    <message>
+        <source>followed you</source>
+        <translation>zaczął(-ęła) Cię śledzić</translation>
+    </message>
+</context>
+<context>
+    <name>MyList</name>
+    <message>
+        <source>Load more</source>
+        <translation>Załaduj więcej</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>Ustawienia</translation>
+    </message>
+    <message>
+        <source>Loading</source>
+        <translation>Ładowanie</translation>
+    </message>
+    <message>
+        <source>please wait...</source>
+        <translation>proszę czekać…</translation>
+    </message>
+</context>
+<context>
+    <name>Profile</name>
+    <message>
+        <source>Unfollow</source>
+        <translation>Przestań śledzić</translation>
+    </message>
+    <message>
+        <source>Follow request sent!</source>
+        <translation>Wysłano prośbę o możliwość śledzenia!</translation>
+    </message>
+    <message>
+        <source>Following</source>
+        <translation>Śledzisz</translation>
+    </message>
+    <message>
+        <source>Mute</source>
+        <translation>Wycisz</translation>
+    </message>
+    <message>
+        <source>Unmute</source>
+        <translation>Unmute</translation>
+    </message>
+    <message>
+        <source>Unblock</source>
+        <translation>Unblock</translation>
+    </message>
+    <message>
+        <source>Block</source>
+        <translation>Block</translation>
+    </message>
+    <message>
+        <source>Statuses</source>
+        <translation>Statuses</translation>
+    </message>
+    <message>
+        <source>Favourites</source>
+        <translation>Favourites</translation>
+    </message>
+    <message>
+        <source>Follow</source>
+        <translation>Follow</translation>
+    </message>
+    <message>
+        <source>Summary</source>
+        <translation>Summary</translation>
+    </message>
+    <message>
+        <source>Followers</source>
+        <translation>Followers</translation>
+    </message>
+</context>
+<context>
+    <name>Settings</name>
+    <message>
+        <source>Settings</source>
+        <translation type="unfinished">Ustawienia</translation>
+    </message>
+    <message>
+        <source>Remove Account</source>
+        <translation>Usuń konto</translation>
+    </message>
+    <message>
+        <source>Add Account</source>
+        <translation>Dodaj konto</translation>
+    </message>
+    <message>
+        <source>Authorize this app to use your Mastodon account in your behalf</source>
+        <translation>Uwierzytelnij tę aplikację, aby mogła używać Mastodona w Twoim imieniu</translation>
+    </message>
+    <message>
+        <source>Load images in toots</source>
+        <translation>Ładuj obrazy we wpisach</translation>
+    </message>
+    <message>
+        <source>Deauthorize this app and remove your account</source>
+        <translation>Cofnij uwierzytelnienie tej aplikacji i usuń swoje konto</translation>
+    </message>
+    <message>
+        <source>Credits</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Translate</source>
+        <translation>Tłumacz</translation>
+    </message>
+    <message>
+        <source>Use Transifex to help with app translation to your language</source>
+        <translation>Użyj platformy Transifex, aby pomóc w tłumaczeniu tej aplikacji na Twój język</translation>
+    </message>
+    <message>
+        <source>Disable this option if you want to preserve your data connection</source>
+        <translation>Wyłącz tę opcję, aby oszczędzać zużycie danych</translation>
+    </message>
+    <message>
+        <source>UI/UX design and development</source>
+        <translation>Rozwój i projektowanie UI/UX</translation>
+    </message>
+</context>
+<context>
+    <name>Toot</name>
+    <message>
+        <source>boosted</source>
+        <translation>podbił(a)</translation>
+    </message>
+    <message>
+        <source>favourited</source>
+        <translation>dodał(a) do ulubionych</translation>
+    </message>
+    <message>
+        <source>followed you</source>
+        <translation>zaczął(-ęła Cię śledzić)</translation>
+    </message>
+</context>
+<context>
+    <name>VisualContainer</name>
+    <message>
+        <source>Unboost</source>
+        <translation>Cofnij podbicie</translation>
+    </message>
+    <message>
+        <source>Boost</source>
+        <translation>Podbij</translation>
+    </message>
+    <message>
+        <source>Unfavorite</source>
+        <translation>Usuń z ulubionych</translation>
+    </message>
+    <message>
+        <source>Favorite</source>
+        <translation>Dodaj do ulubionych</translation>
+    </message>
+</context>
+</TS>
